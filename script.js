78777576945c28f452fe42d80af021a8e7ba3b82
Stevr82 <stefano.bresolin.vr@gmail.com>
2025-09-15 15:24:42 +0200

    script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const fileIDs = {
-  gennaio: 'INSERISCI_ID_DI_GENNAIO',
-  febbraio: 'INSERISCI_ID_DI_FEBBRAIO',
-  marzo: 'INSERISCI_ID_DI_MARZO',
-  aprile: 'INSERISCI_ID_DI_APRILE',
-  maggio: 'INSERISCI_ID_DI_MAGGIO',
-  giugno: 'INSERISCI_ID_DI_GIUGNO',
-  luglio: 'INSERISCI_ID_DI_LUGLIO',
-  agosto: 'INSERISCI_ID_DI_AGOSTO',
-  settembre: 'INSERISCI_ID_DI_SETTEMBRE',
-  ottobre: 'INSERISCI_ID_DI_OTTOBRE',
-  novembre: 'INSERISCI_ID_DI_NOVEMBRE',
-  dicembre: 'INSERISCI_ID_DI_DICEMBRE'
+  gennaio: '1kEsCsEKl6C9ZGkdOpzLxE2bSMWnkH_r3F34Yvk2zDFU',
+  febbraio: '1wh9JZy0kTksXXtu2rvDTRXceh7RQ-NXcdulw6in4AjI',
+  marzo: '1SNHIGxX6jtKB2gK03e2m0YO9Oj-wd0KTTHAmyiJG3_E',
+  aprile: '1yyH1k3dBSfQn6XiTLrmPJ1KeUORKCnLvYBvanjogVEk',
+  maggio: '17ejoWO2hVNekJpwCwbwgcVGmmqwuFqeC6SzoLhMxC6Q',
+  giugno: '1ACQDVLh1Whdpm0vmmr1c4ZvNBv-4dFmS_oCDKa1pY3g',
+  luglio: '1c_K7PCXRPtn3v5IsYr2qaPoFAgWUiHkc05j7roOD8_E',
+  agosto: '1TWk5RWnUn--cIkkFBZFFzBEvdziBoX73GpyPngir1SE',
+  settembre: '1Pbwo64rcv0RGzJiDa84ev8hRwUzKydrlYNRGgI2sjaA',
+  ottobre: '1M37_o_PvujXozP_461GZgyeGB50X9Mc5RoNi-I5rBbI',
+  novembre: '1Xih-zeZPD3Wgo5wSY7IzP9GAhkXSaScU0cJJMFm_xGM',
+  dicembre: '1Sd5lssnfPF0okKLMD_mcC7I08lwfc99YdoQL5I7i-9o'
 };
 
 function caricaExcel() {
